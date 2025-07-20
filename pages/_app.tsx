a3acf3364ec85c47587fd09091a3e414de46341b
Ng Kah Lok <ngkahlok0407@gmail.com>
2025-07-20 17:44:08 +0800

    Changed Font awesome for icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import '@/styles/globals.css'
 import ResponsiveBackground from '@/components/Responsivebackground';
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
